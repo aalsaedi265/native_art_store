48ed5f4bb334d6374c333d31e2aa52596816290f
aalsaedi265 <aalsaedi265@gmail.com>
2022-12-12 16:16:04 -0600

    the search tool palce in, also not operational

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,6 +1,6 @@
 import { View, Text, Image, TextInput } from 'react-native'
 import React from 'react'
-import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { COLORS, SIZES, SHADOWS, assets, FONTS } from "../constants";
 
 
 const HomeHeader = () => {
@@ -34,6 +34,50 @@ const HomeHeader = () => {
             resizeMode='contain'
             style={{position:'absolute',
              width:15, height:15, right:0, bottom:0}}
+            />
+          </View>
+        </View>
+        <View style={{marginVertical:SIZES.font}}>
+          <Text
+          style={{
+            fontFamily:FONTS.regular,
+            color:COLORS.white,
+            fontSize:SIZES.small
+          }}
+          >Hello, Broly</Text>
+
+          <Text
+          style={{
+            fontFamily: FONTS.bold,
+            color:COLORS.white,
+            marginTop:SIZES.base,
+            fontSize: SIZES.large,
+            marginTop: SIZES.base/2
+          }}
+          > Please don't take a picture</Text>
+        </View>
+        <View style={{marginTop:SIZES.font}}>
+          <View style={{
+            width:'100%',
+            borderRadius: SIZES.font,
+            backgroundColor: COLORS.gray,
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingHorizontal: SIZES.font,
+            paddingVertical:SIZES.small
+          }}>
+            <Image
+            source={assets.search}
+            resizeMode='contain'
+            style={{width:20,
+                    height:20,
+                    marginRight: SIZES.base
+                    }}
+            />
+            <TextInput
+            placeholder='Search Art'
+            style={{flex:1, color:'pink', fontFamily:'InterBold'}}
+            onChangeText={()=>{}}
             />
           </View>
         </View>
